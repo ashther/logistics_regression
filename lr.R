3db14968d40f3772946c38645119a4ccc2c3376e
Sun Lijun <ashther@qq.com>
2016-01-19 10:02:32 +0800

    增加了正则化

--- lr.R
+++ lr.R
@@ -1,3 +1,5 @@
+require(magrittr)
+require(ROCR)
 
 if (!exists('verify') | !exists('train')) {
     source('dataSource.R')
@@ -28,6 +30,26 @@ gradAscent <- function(train, alpha, max_cycles) {
     return(weight)
 }
 
+ga_test <- function(train, alpha, max_cycles, lambda) {
+    n <- dim(train)[2] - 1
+    m <- dim(train)[1]
+    
+    # 获取分类结果，重新定义训练集
+    label <- train[, n + 1]
+    train <- train[, 1:n]
+    
+    weight <- rep(1, n)
+    
+    for (i in 1:max_cycles) {
+        h <- train %*% weight %>% 
+            sigmoid()
+        error <- label - h
+        weight <- weight + alpha * (t(train) %*% error - lambda * weight)
+    }
+    
+    return(weight)
+}
+
 lrPredict <- function(test, weight) {
     result <- apply(test, 1, function(t){
         temp <- ifelse(sigmoid(t %*% weight) > 0.5, 1, 0)
@@ -36,17 +58,26 @@ lrPredict <- function(test, weight) {
     return(result)
 } 
 
-lrCompare <- function(train, verify, alpha, max_cycles) {
+lrCompare <- function(train, verify, alpha, max_cycles, lambda) {
     result <- list()
     
     pre <- gradAscent(train, alpha, max_cycles) %>%
         lrPredict(verify[, 1:(ncol(verify) - 1)], .)
+    pre_with_lambda <- ga_test(train, alpha, max_cycles, lambda) %>%
+        lrPredict(verify[, 1:(ncol(verify) - 1)], .)
     
     result[['accuracy']] <- mean(pre == verify[, ncol(verify)])
-    result[['auc']] <- prediction(pre, verify[, ncol(verify)]) %>%
-        performance('auc') %>% 
-        `@`(y.values) %>% 
-        `[[`(1)
+    result[['auc']] <- tryCatch({prediction(pre, verify[, ncol(verify)]) %>%
+            performance('auc') %>% 
+            `@`(y.values) %>% 
+            `[[`(1)}, error = function(e)return(e))
+    
+    result[['accuracy_lambda']] <- mean(pre_with_lambda == verify[, ncol(verify)])
+    result[['auc_lambda']] <- tryCatch({prediction(pre_with_lambda, verify[, ncol(verify)]) %>%
+            performance('auc') %>% 
+            `@`(y.values) %>% 
+            `[[`(1)}, error = function(e)return(e))
+    
     return(result)
 }
 
